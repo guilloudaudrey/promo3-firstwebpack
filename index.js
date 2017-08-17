8ff87e21a4e2f9efd7a4deb250bb033e3017662f
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-08-17 19:53:56 +0200

    a

---
+++ index.js
@@ -0,0 +1,16 @@
+//En javascript modulaire, pour pouvoir utiliser une library
+//ou un autre fichier, on doit importer celui ci, soit
+//via import .. from ''
+import $ from 'jquery';
+
+//soit via un require
+//const $ = require('jquery');
+
+
+$(document).ready(function() {
+    $('#para').css({
+        color: 'red',
+        background: 'blue',
+        fontSize: 50
+    });
+});
